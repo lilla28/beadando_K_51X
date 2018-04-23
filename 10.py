3a782572f7c8fb7ef8ac6b0c90c23addf9c752f3
lilla28 <36889371+lilla28@users.noreply.github.com>
2018-04-23 21:21:13 +0200

    10.feladat

--- 10.py
+++ 10.py
@@ -1,14 +1,15 @@
 def oszthato():
-    n=1
+    oszto=1
     szam=1
-    db=0
-    while n!=20:
-        if szam%n>0:
+    while oszto!=20:
+        db=0
+        if szam%oszto>0:
             szam=szam+1
-        elif szam%n==0:
+        if szam%oszto==0:
             db=db+1
-            n = n + 1
-            break
-        return szam
-
-print('A legkisebb pozitív egész szám, amely osztható 1-20-ig:',oszthato())
+            oszto = oszto + 1
+        print(db)
+        # if db==20:
+        #     return szam
+oszthato()
+# print('A legkisebb pozitív egész szám, amely osztható 1-20-ig:',oszthato())
