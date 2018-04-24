7f12d7d58909972f4b37d0d5c02c2b893ee8c54b
lilla28 <36889371+lilla28@users.noreply.github.com>
2018-04-24 14:41:03 +0200

    10.feladat harmadik probalkozas

--- 10_2.py
+++ 10_2.py
@@ -8,3 +8,5 @@ def oszto():#egy fv letrehozasa
 
 print(oszto())#fuggveny meghivasa
 #problema a programmal: a lassu futas es a feltetel 
+
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
