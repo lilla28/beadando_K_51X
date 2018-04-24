37eb9cea92eabff064adc03a458062c96e0c4d81
lilla28 <36889371+lilla28@users.noreply.github.com>
2018-04-24 14:33:54 +0200

    10.feladat masodik probalkozas

--- 10_1.py
+++ 10_1.py
@@ -5,6 +5,7 @@ def osthato():
         for i in range(1,21):
             if szam%i==0:
                 db=db+1
+                szam=szam+1
             if szam%i>0:
                 szam=szam+1
         if db==20:
