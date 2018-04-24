63f3063720aa7cea138de4a622ad2f085e1ee7bc
lilla28 <36889371+lilla28@users.noreply.github.com>
2018-04-24 14:37:57 +0200

    12.feladat

--- 12.py
+++ 12.py
@@ -1,3 +1,4 @@
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.What is the 10 001st prime number?
 def prim(n):
     db=1
     for i in range(1,(n//2)+1):
