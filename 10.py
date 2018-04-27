4d680f146d99fe83d6dfd5986d77d57c9f7e29fa
lilla28 <36889371+lilla28@users.noreply.github.com>
2018-04-27 14:15:13 +0200

    10.feladat_kesz

--- 10.py
+++ 10.py
@@ -1,6 +1,6 @@
 #2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? 
 def oszthato():
-    szam=2520
+    szam=1
     while True:
         db=0
         for oszto in range(11,21):
@@ -11,3 +11,4 @@ def oszthato():
         if db==10:
             return szam
 print('a legkisebb szam, amely osztahato 1-20-ig maradék nélkül:',oszthato())
+#kb 40ms futasi ido
