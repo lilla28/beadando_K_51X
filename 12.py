aa9effff54f1067a426d837979eae1674d326332
lilla28 <36889371+lilla28@users.noreply.github.com>
2018-05-09 14:30:21 +0200

    12_feladat_kesz

--- 12.py
+++ 12.py
@@ -1,19 +1,17 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.What is the 10 001st prime number?
-def prim(n):
-    db=1
-    for i in range(1,(n//2)+1):
+def prim(n): #fv a primszamok vizsgalatara
+    for i in range(2,(n//2)+1):
         if n%i==0:
-            db=db+1
-    if db==2:
-        return True
-    return False
+            return False
+    return True
 # print(prim(11))
-def number():
+def number(): #az 10001.primszamot adja vissza
     hanyadik=0
     szam=1
     while hanyadik<10001:
         if prim(szam):
             hanyadik=hanyadik+1
         szam=szam+1
+    return szam
 
 print('a 10001-dik primszam: ',number())
